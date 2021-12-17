c8528a5eb5738015df8ed03cb1cebe3f5d1979de
Crosstyan <cf595833532@gmail.com>
2021-12-18 01:04:59 +0800

    fix: mobile drawer animation

--- src/styles.ts
+++ src/styles.ts
@@ -65,8 +65,8 @@ export const useStyles = makeStyles()((theme: Theme) => ({
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     }),
-    width: theme.spacing(7),
-    display: "none",
+    // width: theme.spacing(7),
+    width: 0,
     [theme.breakpoints.up("sm")]: {
       width: theme.spacing(9),
       display: "flex",
