79750a525728a4af717830d0f0dabd728bf8e4ad
Crosstyan <cf595833532@gmail.com>
2021-12-18 03:25:22 +0800

    fix: move ws handler to realtime

--- src/page/history.tsx
+++ src/page/history.tsx
@@ -4,7 +4,7 @@ import { Plot, Point } from "../components/plot";
 import { CustomTable } from "../components/table";
 import { useStyles } from "../styles"
 import Masonry from '@mui/lab/Masonry';
-import { MQTTTopic } from "../Dashboard"
+import { MQTTTopic } from "./realtime"
 import {
   Typography,
   Button,
@@ -13,8 +13,8 @@ import {
   Stack,
   TextField,
 } from "@mui/material";
-import {config} from "../config";
-import {getUniqueListBy} from "../utils/utils";
+import { config } from "../config";
+import { getUniqueListBy } from "../utils/utils";
 import moment from "moment";
 
 interface DataQuery {
@@ -41,7 +41,7 @@ function dateToDatetimeLocal(date: string | number): string {
   const TimeFormat = 'YYYY-MM-DDTHH:mm'
   return moment(date).format(TimeFormat)
 }
-function respToPoint(res: ResponseRecord): Point{
+function respToPoint(res: ResponseRecord): Point {
   return {
     x: moment(res.timestamp).valueOf(),
     y: res.payload
@@ -52,17 +52,17 @@ interface HistoryChartProps {
   topic: MQTTTopic;
 }
 
-export function HistoryChart({topic}: HistoryChartProps) {
+export function HistoryChart({ topic }: HistoryChartProps) {
   const { classes, cx } = useStyles();
   const paperClass = clsx(classes.paper);
-  const [data, setData] = useState<Point[]>([{ x: Date.now(), y: 0}])
+  const [data, setData] = useState<Point[]>([{ x: Date.now(), y: 0 }])
   // RFC3399
   const [startDate, setStartDate] = useState<string>(dateToDatetimeLocal(Date.now()))
-  const [endDate, setEndDate] = useState<string|null>(null)
+  const [endDate, setEndDate] = useState<string | null>(null)
   const [page, setPage] = useState<number>(1)
   const [isMore, setIsMore] = useState<boolean>(false)
   // TODO: refactor this function to make it out of this component
-  const queryFirstData = (topic: MQTTTopic, startDate:string, endDate:string|null = null) => {
+  const queryFirstData = (topic: MQTTTopic, startDate: string, endDate: string | null = null) => {
     let queryPath: string
     switch (topic) {
       case 'temperature':
@@ -75,13 +75,13 @@ export function HistoryChart({topic}: HistoryChartProps) {
         throw new Error(`Unknown topic: ${topic}`)
     }
     const url = `http://${config.addr}${queryPath}`
-    const query:DataQuery = endDate == null ? {
-        page: 1,
-        start: dateTimeLocalToISO(startDate),
+    const query: DataQuery = endDate == null ? {
+      page: 1,
+      start: dateTimeLocalToISO(startDate),
     } : {
-        page: 1,
-        start: dateTimeLocalToISO(startDate),
-        end: dateTimeLocalToISO(endDate),
+      page: 1,
+      start: dateTimeLocalToISO(startDate),
+      end: dateTimeLocalToISO(endDate),
     }
     console.log(query)
     const rawResponse = fetch(url, {
@@ -100,13 +100,13 @@ export function HistoryChart({topic}: HistoryChartProps) {
         // setPage to 2 then
         setPage(page => 2)
       } else {
-        setData([{x: Date.now(), y: 0}])
+        setData([{ x: Date.now(), y: 0 }])
         setIsMore(false)
       }
     }))
   }
   // TODO: refactor this function to avoid copy and paste
-  const queryMoreData = (topic: MQTTTopic, startDate:string, endDate:string|null = null) => {
+  const queryMoreData = (topic: MQTTTopic, startDate: string, endDate: string | null = null) => {
     let queryPath: string
     switch (topic) {
       case 'temperature':
@@ -119,13 +119,13 @@ export function HistoryChart({topic}: HistoryChartProps) {
         throw new Error(`Unknown topic: ${topic}`)
     }
     const url = `http://${config.addr}${queryPath}`
-    const query:DataQuery = endDate == null ? {
-        page: page,
-        start: dateTimeLocalToISO(startDate),
+    const query: DataQuery = endDate == null ? {
+      page: page,
+      start: dateTimeLocalToISO(startDate),
     } : {
-        page: page,
-        start: dateTimeLocalToISO(startDate),
-        end: dateTimeLocalToISO(endDate),
+      page: page,
+      start: dateTimeLocalToISO(startDate),
+      end: dateTimeLocalToISO(endDate),
     }
     console.log(query)
     const rawResponse = fetch(url, {
@@ -161,13 +161,13 @@ export function HistoryChart({topic}: HistoryChartProps) {
       }
     }))
   }
-  const More = ({topic}:HistoryChartProps)=>{
-    if(isMore){
-      return(<Button onClick={()=>{queryMoreData(topic, startDate, endDate)}}>Get More</Button>)
+  const More = ({ topic }: HistoryChartProps) => {
+    if (isMore) {
+      return (<Button onClick={() => { queryMoreData(topic, startDate, endDate) }}>Get More</Button>)
     } else {
-      return(<Button disabled onClick={()=>{queryMoreData(topic, startDate, endDate)}}>No More</Button>)
+      return (<Button disabled onClick={() => { queryMoreData(topic, startDate, endDate) }}>No More</Button>)
     }
-}
+  }
 
   return (
     <Masonry columns={{ xs: 1, md: 2 }} spacing={{ xs: 1, md: 2 }}>
@@ -185,7 +185,7 @@ export function HistoryChart({topic}: HistoryChartProps) {
             InputLabelProps={{
               shrink: true,
             }}
-            onChange={(e)=>{
+            onChange={(e) => {
               setStartDate(date => e.target.value)
               queryFirstData(topic, e.target.value, endDate)
             }}
@@ -202,7 +202,7 @@ export function HistoryChart({topic}: HistoryChartProps) {
             InputLabelProps={{
               shrink: true,
             }}
-            onChange={(e)=>{
+            onChange={(e) => {
               setEndDate(date => e.target.value)
               queryFirstData(topic, startDate, e.target.value)
             }}
@@ -210,13 +210,13 @@ export function HistoryChart({topic}: HistoryChartProps) {
         </Stack>
       </Paper>
       <Paper className={paperClass}>
-        <Plot data={data} format={(date)=>{
+        <Plot data={data} format={(date) => {
           return moment(date).format("HH:mm:ss")
-        }}/>
+        }} />
       </Paper>
       <Paper className={paperClass}>
         <CustomTable rows={data} />
-        <More topic={topic}/>
+        <More topic={topic} />
       </Paper>
     </Masonry>
   )
